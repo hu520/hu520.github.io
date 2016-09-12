d93260bda7d980db74584bd39913b28531e31012
hu520 <948140127@qq.com>
2016-09-12 21:41:34 +0800

    xiaomi8

--- xiaomi/js/index.js
+++ xiaomi/js/index.js
@@ -13,7 +13,7 @@ window.onload = function (){
 	var timer = 0;
 	for(var i = 0; i < li.length; i++){
 		str += "<a href='javascript:;'></a>"
-	}
+	};
 	navs.innerHTML = str;
 	navsA[0].className = "active";
 	wrap.onmouseover = function() {	
@@ -29,7 +29,7 @@ window.onload = function (){
 			var next =this.index;
 			tab(next);
 		};
-	}
+	};
 	prevBtn.onclick = function() {
 		var next = now - 1;
 		if(next < 0)
@@ -47,7 +47,7 @@ window.onload = function (){
 			next = 0;
 		}
 		tab(next);
-	}
+	};
 	function tab(next) {
 		zIndex++;
 		navsA[now].className = "";
@@ -56,7 +56,7 @@ window.onload = function (){
 		navsA[now].className = "active";
 		startMove(li[now],"opacity",1000,100,"easeOut");
 		li[now].style.zIndex = zIndex;
-	}
+	};
 
 	$('#curr').hover(function(){
 		$(this).find('p').stop().animate({
@@ -68,7 +68,7 @@ window.onload = function (){
 			height:'0',
     		display: 'none',
     		opacity: 0},300)
-	})
+	});
 	 // 收索栏效果
 	 // var tx = $('input')[0];
 	 $('.text').focus(function(){
@@ -76,16 +76,16 @@ window.onload = function (){
 	 	$('.deng').eq($(this).index()).show();
 	 	$('.shouding').eq($(this).index()).hide();
 	 	$('.btn').eq($(this).index()).css({border:'1px solid #ff6700'});
-	 })
+	 });
 	 $('.text').blur(function(){
 	 	$(this).css({border:'1px solid #e0e0e0'});
 	 	$('.shouding').eq($(this).index()).show();
 	 	$('.btn').eq($(this).index()).css({border:'1px solid #e0e0e0'});
 	 	$('.deng').hide();
-	 })
+	 });
 	 $('.deng').on('mouseover',function(){
 	 	$('.deng').show();
-	 })
+	 });
 	 
 	// 列表移入切换效果
 	$("#list1").find('li').mouseover(function(){
@@ -120,10 +120,10 @@ window.onload = function (){
 		
 		$(".navs").find('a').eq(7).mouseover(function(){
 			$('#navlist').stop().animate({height: 0});
-		})
+		});
 		$(".navs").find('a').eq(8).mouseover(function(){
 			$('#navlist').stop().animate({height: 0});
-		})
+		});
 
 		$(".low1").mouseout(function(){
 			$('#navlist').stop().animate({height: 0},200);
@@ -144,35 +144,42 @@ window.onload = function (){
 	// timer=setInterval(function(){
 
 	// },1000)
-	$('#Right').click(function(){
-		$(this).removeClass('Bactive').siblings('a').addClass('Bactive');
-		$('#ul').stop().animate({
-			left:-iwa+14
-		},500);
-	});
+	function auto(id){
+		$(id).find('#Right').click(function(){
+			$(this).removeClass('Bactive').siblings('a').addClass('Bactive');
+			$(id).find('#ul').stop().animate({
+				left:-iwa+14
+			},500);
+		});
 
-	$('#Left').click(function(){
-		$(this).removeClass('Bactive').siblings('a').addClass('Bactive');	
-		$('#ul').stop().animate({
-			left:0
-		},500);
-	});
-		
+		$(id).find('#Left').click(function(){
+			$(this).removeClass('Bactive').siblings('a').addClass('Bactive');	
+			$(id).find('#ul').stop().animate({
+				left:0
+			},500);
+		});
+	};
+	auto('#low3bg1');
+	auto('#low3bg2');
 		var h2s = document.getElementsByTagName('h2');
 		// var h2s = document.getElementsByClassName
 		console.log(h2s)
 		// 购物图片切换图效果
-		$('.low4-top').find('a').mouseover(function(){
-			$(this).find('a').show().siblings().hide();
-			$('.low4list').find('.uls').eq($(this).index()).show().siblings().hide();
-			// $('.low4list').find('.uls').css({margin-left:234})
-			$('.main').show();
-		})
-
+		function drag(id){
+			$(id).find('.low4-top a').mouseover(function(){
+				$(this).addClass('active').siblings('a').removeClass('active');
+				$(id).find('.low4list .uls').eq($(this).index()).show().siblings().hide();
+				// $('.low4list').find('.uls').css({margin-left:234})
+				$(id).find('.low4-img').show();
+			});
+		};
+		drag('#low4-two1');
+		drag('#low4-two2');
+		drag('#low4-two3');
 		// 搭配购物图片弹出文字效果
 		$('.uls').find('li').mouseover(function(){
 			$(this).find('.skill').stop().animate({bottom:0,opacity:1},200);
-		})
+		});
 		$('.uls').find('li').mouseout(function(){
 			$(this).find('.skill').stop().animate({bottom:-80,opacity:0},200);
 		});
@@ -186,7 +193,7 @@ window.onload = function (){
 				'<strong>'+datas[i].st+'</strong></a>'+
 				'<p>'+datas[i].tx+'</p>'+
 				'<span>'+datas[i].sp+'</span></li>';
-		}
+		};
 		// plis.html(html);
 		// $('.plis')[0].innerHTML = html;
 
@@ -198,12 +205,12 @@ window.onload = function (){
 			$(this).find('.bookbtn').stop().animate({opacity:1});
 		},function(){
 			$(this).find('.bookbtn').stop().animate({opacity:0});
-		})
+		});
 		$(id).find('.bookright').click(function(){
 			num++;
 			if(num == $(id).find('.bookli').size()){
 				num = 0;
-			}
+			};
 			$(id).find('.booknav').find('a').eq(num).addClass('booktive').siblings('a').removeClass('booktive');
 			$(id).find('.book').stop().animate({
 				left:-iw*num
@@ -228,135 +235,9 @@ window.onload = function (){
 				left:-iw*num
 			});
 		});
-	}
+	};
 	fn('#bookbox1');
 	fn('#bookbox2');
 	fn('#bookbox3');
 	fn('#bookbox4');
-		// 1111111111111
-	// $('#right1').click(function(){
-	// 	num++;
-	// 	if(num ==4){
-	// 		num = 0;
-	// 	}
-	// 	$('#nav1').find('a').eq(num).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	$('#book1').stop().animate({
-	// 		left:-iw1*num
-	// 	});
-	// });
-
-	// $('#left1').click(function(){
-	// 	num--;
-	// 	if(num < 0){
-	// 		num = 3;
-	// 	}
-	// 	$('#nav1').find('a').eq(num).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	$('#book1').stop().animate({
-	// 		left:-iw1*num
-	// 	});
-	// });
-	
-	// $('#nav1').find('a').click(function(){
-	// 	$(this).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	num = $(this).index();
-	// 	$('#book1').stop().animate({
-	// 		left:-iw1*num
-	// 	});
-	// });
-	// 	// 2222222222222222222
-	// 	var iw2 = $('#bookli2').eq(0).width();
-	// var num2 = 0;
-	// $('#right2').click(function(){
-	// 	num2++;
-	// 	if(num2 ==4){
-	// 		num2 = 0;
-	// 	}
-	// 	$('#nav2').find('a').eq(num2).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	$('#book2').stop().animate({
-	// 		left:-iw2*num2
-	// 	});
-	// });
-
-	// $('#left2').click(function(){
-	// 	num2--;
-	// 	if(num2 < 0){
-	// 		num2 = 3;
-	// 	}
-	// 	$('#nav2').find('a').eq(num2).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	$('#book2').stop().animate({
-	// 		left:-iw2*num2
-	// 	});
-	// });
-	
-	// $('#nav2').find('a').click(function(){
-	// 	$(this).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	num2 = $(this).index();
-	// 	$('#book2').stop().animate({
-	// 		left:-iw2*num2
-	// 	});
-	// });
-	// // 33333333333333333333
-	// var iw3 = $('#bookli3').eq(0).width();
-	// var num3 = 0;
-	// $('#right3').click(function(){
-	// 	num3++;
-	// 	if(num3 ==4){
-	// 		num3 = 0;
-	// 	}
-	// 	$('#nav3').find('a').eq(num3).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	$('#book3').stop().animate({
-	// 		left:-iw3*num3
-	// 	});
-	// });
-
-	// $('#left3').click(function(){
-	// 	num3--;
-	// 	if(num3< 0){
-	// 		num3 = 3;
-	// 	}
-	// 	$('#nav3').find('a').eq(num3).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	$('#book3').stop().animate({
-	// 		left:-iw3*num3
-	// 	});
-	// });
-	
-	// $('#nav3').find('a').click(function(){
-	// 	$(this).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	num3 = $(this).index();
-	// 	$('#book3').stop().animate({
-	// 		left:-iw3*num3
-	// 	});
-	// });
-	// // 4444444444444444444444444444
-	// var iw4 = $('#bookli4').eq(0).width();
-	// var num4 = 0;
-	// $('#right4').click(function(){
-	// 	num4++;
-	// 	if(num4 ==4){
-	// 		num4 = 0;
-	// 	}
-	// 	$('#nav4').find('a').eq(num4).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	$('#book4').stop().animate({
-	// 		left:-iw4*num4
-	// 	});
-	// });
-
-	// $('#left4').click(function(){
-	// 	num4--;
-	// 	if(num4 < 0){
-	// 		num4 = 3;
-	// 	}
-	// 	$('#nav4').find('a').eq(num4).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	$('#book4').stop().animate({
-	// 		left:-iw4*num4
-	// 	});
-	// });
-	
-	// $('#nav4').find('a').click(function(){
-	// 	$(this).addClass('booktive').siblings('a').removeClass('booktive');
-	// 	num4 = $(this).index();
-	// 	$('#book4').stop().animate({
-	// 		left:-iw4*num4
-	// 	});
-	// });
 };
